865d1782734e2716131c92219ee08f26251e62dc
Anton2725 <anton545@mail.ru>
2021-08-03 21:18:19 +0500

    Добавил перечисление DaysOfWeek

--- Program.cs
+++ Program.cs
@@ -18,4 +18,15 @@ namespace FirstApp
             Console.ReadKey();
         }
     }
+
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
 }
