932af75c9c76db29c39c66307d1f43478013a3b9
Anton2725 <anton545@mail.ru>
2021-08-09 14:33:19 +0500

    Задание 4.1.17

--- Program.cs
+++ Program.cs
@@ -10,12 +10,40 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string myName;
+            /*string myName;
             myName = "Anton";
 
-            Console.WriteLine(myName);
+            Console.WriteLine("Мое имя {0}", myName);
 
             Console.ReadKey();
+            */
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
         }
     }
 
